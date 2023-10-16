04e7791ac4b8af5692bc70fb174d8d86638cb75e
Tictaker3042 <il.lebed2011.gmail.com>
2023-10-16 15:56:03 +0300

    master

---
+++ udp_client.py
@@ -0,0 +1,17 @@
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+
+
+udpCliSock = socket(AF_INET, SOCK_DGRAM)
+while True:
+    message = input('Введите сообщение: ')
+    udpCliSock.sendto(message.encode('utf-8'), ADDR)
+    data, addr = udpCliSock.recvfrom(BUFSIZE)
+    print('Получено сообщение:', data.decode('utf-8'), 'от', addr)
+
+    udpCliSock.close()
